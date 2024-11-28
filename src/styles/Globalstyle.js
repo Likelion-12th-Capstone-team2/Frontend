21d072db52cd3390ae013893ebb1906b42c1749a
s-uxun <serenasol@ewha.ac.kr>
2024-11-28 17:20:51 +0900

    Design: Font, GlobalStyle

--- src/styles/Globalstyle.js
+++ src/styles/Globalstyle.js
@@ -1,8 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import '../fonts/font.css';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+
+  :root{
+    font-family: 'Pridi', sans-serif;
+  }
 
   *{
     box-sizing: border-box;
