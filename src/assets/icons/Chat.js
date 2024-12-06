a8ed787be61cb2d3c01fb9389bdc64898bd0be90
s-uxun <serenasol@ewha.ac.kr>
2024-12-06 18:16:56 +0900

    Feat: Publishing SignIn.jsx

---
+++ src/assets/icons/Chat.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+const SvgChat = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 20 18"
+    {...props}
+    width="1.1rem"
+  >
+    <path
+      fill="#000"
+      fillRule="evenodd"
+      d="M10.402.037c-5.2 0-9.42 3.277-9.42 7.308 0 2.514 1.63 4.709 4.117 6.046l-1.046 3.833a.386.386 0 0 0 .594.414l4.577-3.042c.387 0 .782.066 1.178.066 5.2 0 9.42-3.277 9.42-7.317S15.601.037 10.401.037"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgChat;
--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Chat } from '@/assets/icons';
 
 const SignIn = () => {
   return (
@@ -7,7 +8,17 @@ const SignIn = () => {
         <Title>Log in to I WANT IT!</Title>
         <Content>
           <p>Email</p>
+          <Input type="email" />
           <p>Password</p>
+          <Input type="password" />
+          <Button>
+            <BtnIn>Sign in</BtnIn>
+            <BtnIn style={{ backgroundColor: 'black' }}>Log in</BtnIn>
+          </Button>
+          <BtnKakao>
+            <Chat />
+            <KakaoP>Log in with Kakao</KakaoP>
+          </BtnKakao>
         </Content>
       </Container>
     </Wrapper>
@@ -19,6 +30,7 @@ export default SignIn;
 const Container = styled.div`
   width: 41rem;
   border: 1px solid black;
+  margin: 7.5rem 2.5rem;
 `;
 
 const Title = styled.p`
@@ -29,12 +41,52 @@ const Title = styled.p`
   background-color: black;
 `;
 
-const Content = styled.div``;
+const Content = styled.div`
+  background-color: ${({ theme }) => theme.color.mint};
+  padding: 2.63rem 2.38rem;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 3rem;
+  padding: 0 1rem;
+  margin: 0.62rem 0 1rem;
+  ${({ theme }) => theme.font.common_input}
+`;
+
+const Button = styled.div`
+  display: flex;
+  width: fit-content;
+  margin: 0.5rem auto;
+  gap: 0.88rem;
+`;
+
+const BtnIn = styled.button`
+  all: unset;
+  cursor: pointer;
+  ${({ theme }) => theme.font.m_btn}
+  color: white;
+  background-color: ${({ theme }) => theme.color.orange};
+  padding: 0.25rem 0.75rem;
+`;
+
+const BtnKakao = styled.div`
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+  margin-top: 1rem;
+  border: 1px solid black;
+  background: linear-gradient(90deg, #87dbe9 0%, #fff 125.31%);
+`;
+
+const KakaoP = styled.p`
+  ${({ theme }) => theme.font.common_input};
+  color: black;
+`;
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 2rem 2.5rem;
   height: 100vh;
   background-color: ${({ theme }) => theme.color.mint};
   background-image: linear-gradient(
