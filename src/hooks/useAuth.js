123c62fee8619878cb093a69141a639da2f1b761
s-uxun <serenasol@ewha.ac.kr>
2024-12-29 12:51:41 +0900

    Fix: useAuth & sign up Error

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export const useAuth = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
   const isAuthenticated = !!localStorage.getItem('token');
   const id = localStorage.getItem('id');
 
